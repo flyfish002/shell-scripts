2c0f274fbff3717354a7f56cb0d17309feb019b1
flyfish002 <2568424253@qq.com>
2021-09-04 16:32:54 +0800

    add  ar miner

--- start-ar-miner.sh
+++ start-ar-miner.sh
@@ -1,1 +1,17 @@
+#!/bash/sh
 
+NAME='arweave-miner-linux-v1.0.7.5'
+#NAME='x-proxy-linux-amd64'
+ProcessIDs=`ps -ef | grep "$NAME"  | grep -v "grep" | awk '{print $2}'`
+#echo $ProcessIDs
+
+if [ !$ProcessIDs ];then
+  for id in  $ProcessIDs
+     do 
+        kill -9 $id        
+        echo "killed  $id"
+     done
+fi
+
+cd   /data
+nohup   ./arweave-miner-linux-v1.0.7.5 >>  ar-miner-output.log &
