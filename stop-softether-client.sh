b06ec23abfba04c254aaf31a215afca216046348
flyfish002 <2568424253@qq.com>
2021-11-27 11:01:53 +0800

    Update stop-softether-client.sh

--- stop-softether-client.sh
+++ stop-softether-client.sh
@@ -1,9 +1,13 @@
 #!  /bin/bash
 
+
+source /etc/profile
+source ~/.bash_profile
+
+
 vpn_nic_name="cz-eth-1"
 client_path="/root/vpnclient/"
 vpn_name="cz-vpn-1"
-
 
 $client_path./vpncmd /CLIENT localhost /CMD accountdisconnect $vpn_name
 $client_path./vpncmd /CLIENT localhost /CMD accountdelete $vpn_name
