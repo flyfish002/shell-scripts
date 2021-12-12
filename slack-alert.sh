5660cc7642da38bf064585ab8c46469f63362a38
flyfish002 <2568424253@qq.com>
2021-12-12 11:31:31 +0800

    change webhook token

--- slack-alert.sh
+++ slack-alert.sh
@@ -5,12 +5,12 @@
 #goal:   user slack webhook  alert  eth cluster
 
 curl -X POST --data-urlencode \
-"payload={\"channel\": \"#alert-test\", \
+"payload={ \
+\"channel\": \"#alert-test\", \
 \"username\": \"webhookbot\", \
-\"text\": \"注意，fil 集群有问题啦！\"}" \
-https://hooks.slack.com/services/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
-
-
+\"text\": \"注意，fil 集群有问题啦！\" \
+}" \
+https://hooks.slack.com/services/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
 
 
 
