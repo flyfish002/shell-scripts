4d6b43477cabfdab3eb7eb3d70c8a606b4bdb7c7
flyfish002 <2568424253@qq.com>
2021-12-12 10:11:36 +0800

    wq

--- slack-alert.sh
+++ slack-alert.sh
@@ -1,3 +1,16 @@
 #! /bin/bash
 
-echo "this is test"
+#author: james jia
+#date:   2021-12-12
+#goal:   user slack webhook  alert  eth cluster
+
+curl -X POST --data-urlencode \
+"payload={\"channel\": \"#alert-test\", \
+\"username\": \"webhookbot\", \
+\"text\": \"注意，fil 集群有问题啦！\"}" \
+https://hooks.slack.com/services/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
+
+
+
+
+
